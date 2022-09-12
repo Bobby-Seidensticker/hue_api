5e6cf3acd795cbfb1bde50847d88a3d4b1901c57
Bobby Seidensticker <bseidensticker@gmail.com>
2022-09-12 00:48:41 -0700

    Sets lights to appropriate temp and brightness depending on time of day.  Need to get it running on cronjob or forever

--- app.dart
+++ app.dart
@@ -6,6 +6,54 @@ import 'dart:developer';
 //"min":153,"max":454
 var appropriateHourToColor = {};
 
+int appropriateCt(int mins) {
+  final pins = {
+    0: 454,
+    6: 153,
+    12: 153,
+    17: 250,
+    29: 250,
+    24: 454,
+  };
+
+  return between(pins, mins);
+}
+
+int appropriateBri(int mins) {
+  final pins = {
+    0: 1,
+    3: 1,
+    4: 60,
+    6: 255,
+    20: 255,
+    24: 1
+  };
+
+  return between(pins, mins);
+}
+
+int between(Map<int, int> pins, int mins) {
+  var before = -1;
+  var after = -1;
+  for (final key in pins.keys) {
+    if (key * 60 <= mins) {
+      before = key;
+    }
+    if (key * 60 > mins) {
+      after = key;
+      break;
+    }
+  }
+
+  //print('mins $mins, $before ${pins[before]} $after ${pins[after]} ');
+
+  final slope = (pins[after] - pins[before]) / ((after - before) * 60);
+  final result = pins[before] + slope * (mins - before*60);
+  print('${mins/60}, $slope, $result');
+  return result.floor();
+}
+
+
 void main() async {
   for (var i = 0; i < 24; i++) {
     if (i < 7) {
@@ -62,14 +110,9 @@ void main() async {
   // }
 
 
-  //await rename('Hue ambiance downlight 2', 'Living room light 1');
-    await setAppropriateColor();
+  //await rename('Hue ambiance lamp 8', 'Nursery');
+  await setAppropriateColor();
 
-  final now = DateTime.now();
-
-  print(now.hour);
-
-  
 
 
 
@@ -89,15 +132,20 @@ void setAppropriateColor() async {
   var response = await http.get(url);
   final r = jsonDecode(response.body);
   final now = DateTime.now();
+  final mins = now.hour * 60 + now.minute;
+  //final mins = 12*60;
 
   for (final key in r.keys) {
+    if (r[key]['name'] == 'Nursery') continue;
+
     var url = Uri.http('192.168.0.3', '/api/q14kgE9c6eZ6ja35eAH3DSByiYVhhc14bRLQeYC1/lights/$key/state');
-    print("yo ${r[key]['name']} ${r[key]['state']}");
+
+    //print("yo ${r[key]['name']} ${r[key]['state']}");
     if (r[key]['state']['on']) {
       print("${r[key]['name']} is on");
-      var response = await http.put(url, body: '{"ct":${appropriateHourToColor[now.hour]}}');
+      var response = await http.put(url, body: '{"ct":${appropriateCt(mins)}, "bri": ${appropriateBri(mins)}, "transitiontime": 5}');
+      print('    ${response.body}');
     }
-        var response = await http.put(url, body: '{"name":"Living room light"}');
   }
 }
 
